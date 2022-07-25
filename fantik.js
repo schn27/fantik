b6d81a06dfe2c0dd14b7f15baf5744fe1da3b401
Aleksandr Malikov <schn27@gmail.com>
2022-07-25 18:57:03 +0300

    implement number of points optimization

--- fantik.js
+++ fantik.js
@@ -30,10 +30,10 @@ function calc(terrain) {
 	const diffN = g_config.minVz / g_config.speed * g_config.step;
 	const terrainValues = terrain.getValues(g_config.step).map(h => h + g_config.followH);
 
-	const {path, stat} = getPathWithStat(terrainValues, diffP, diffN, g_config.tolerance);
+	const {path, breakPoints, stat} = getPathWithStat(terrainValues, diffP, diffN, g_config.tolerance);
 
+	drawPath(path, breakPoints, terrain.getScope());
 	document.getElementById('stat').innerHTML = stat;
-	drawPath(g_config.optimize ? optimizePath(path) : path, terrain.getScope());
 }
 
 function getPathWithStat(terrainValues, diffP, diffN, tolerance) {
@@ -82,16 +82,86 @@ function getPathWithStat(terrainValues, diffP, diffN, tolerance) {
 		}
 	}
 
+	const limits = terrainValues.map(v => [v, v + tolerance]);
+	const optimized = optimizePath(path, limits, diffP, diffN);
+
 	return {
-		path: path,
-		stat: path.filter((e, i) => e - terrainValues[i] <= tolerance).length / path.length * 100
+		path: optimized.path,
+		breakPoints: optimized.breakPoints,
+		stat: path.filter((e, i) => e <= limits[i][1]).length / path.length * 100
 	};
 }
 
-function optimizePath(path) {
+function optimizePath(path, limits, diffP, diffN) {
+	path = optimizePathPass1(path, limits, diffP, diffN);
+	path = optimizePathPass2(path, limits, diffP, diffN);
+	return {path: path, breakPoints: getBreakPoints(path)};
+}
+
+function optimizePathPass1(path, limits, diffP, diffN) {
+	path = [...path];
+
+	let prevDiff = path[1] - path[0];
+
+	for (let i = 2; i < path.length - 1; ++i) {
+		const newValue = path[i - 1] + prevDiff;
+		const isInLimit = (newValue >= path[i]) && (newValue <= limits[i][1]);
+
+		let tail = i + 1;
+		let diff = undefined;
+		let isNextOk = false;
+
+		for (; isInLimit && !isNextOk && tail < path.length; ++tail) {
+			diff = (path[tail] - newValue) / (tail - i);
+			isNextOk = (diff >= diffN) && (diff <= diffP);
+		}
+
+		if (isInLimit && isNextOk) {
+			for (let j = i; j < tail; ++j) {
+				path[j] = newValue + (j - i) * diff;
+			}
+		} else {
+			prevDiff = path[i] - path[i - 1];
+		}
+	}
+
+	return path;
+}
+
+function optimizePathPass2(path, limits, diffP, diffN) {
+	let from = 0;
+
+	while (from < path.length - 1) {
+		let to = path.length - 1;
+
+		for (let ok = false; !ok && to > from + 1; --to) {
+			const diff = (path[to] - path[from]) / (to - from);
+
+			if (diff >= diffN && diff <= diffP) {
+				ok = true;
+				const newPath = [...path];
+
+				for (let i = from + 1; ok && i < to; ++i) {
+					const newValue = newPath[from] + (i - from) * diff;
+					newPath[i] = newValue;
+					ok &= (newValue >= limits[i][0]) && (newValue <= limits[i][1]);
+				}
+
+				if (ok) {
+					path = newPath;
+				}
+			}
+		}
+
+		from = to;
+	}
+
+	return path;
+}
+
+function getBreakPoints(path) {
 	return path.map((e, i) =>
-		((i == 0 || i == path.length - 1) || 
-			(Math.abs(2 * e - path[i - 1] - path[i + 1]) > 0.001)) ? e : undefined);
+		(i == 0) || (i == path.length - 1) || (Math.abs(2 * e - path[i - 1] - path[i + 1]) > 1e-6));
 }
 
 function readForm() {
@@ -102,7 +172,6 @@ function readForm() {
 		followH: +document.getElementById('followH').value,
 		tolerance: +document.getElementById('tolerance').value,
 		step: +document.getElementById('step').value,
-		optimize: document.getElementById('optimize').checked
 	};
 }
 
@@ -240,7 +309,7 @@ function draw(terrain) {
 	ctx.restore();
 }
 
-function drawPath(path, scope) {
+function drawPath(path, breakPoints, scope) {
 	const canvas = document.getElementById('canvas');
 	const ctx = canvas.getContext('2d');
 	ctx.strokeStyle = '#FF00FF';
@@ -253,7 +322,7 @@ function drawPath(path, scope) {
 	let prev = undefined;
 
 	path.forEach((e, i) => {
-		if (e) {
+		if (breakPoints[i]) {
 			const x = i * g_config.step / scaleX;
 			const y = canvas.height - e / scaleY;
 
